23916571909ca82164ed8594288d5034345c427a
Oxana <oxanal3891@gmail.com>
2023-12-12 01:53:33 +0000

    Added AddRows and Colodoce functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,58 @@
+//Add date display
+
 $(document).ready(function displayDay() {
   $('#currentDay').text(dayjs().format('dddd, MMMM D'));
 })
 
 
+//Create an array of planner hours in DayJs format from 9am to 5pm
+
 var emptyTimeArray = Array.from(new Array(18));
 
-let plannerHours = emptyTimeArray.map((x, i) => {
-  let hour = dayjs().hour(i).format('H A');
+function converttoDayJs(x, i) {
+  let hour = dayjs().hour(i).format('h A');
   return { hour };
-});
+};
+let plannerHours = emptyTimeArray.map(converttoDayJs);
 
 plannerHours.splice(0, 9);
-console.log(plannerHours);
+
+console.log(plannerHours)
+
+//Create a row for each planner hour
+
+let store = window.localStorage;
+
+function addRows() {
+  let grid = $(
+    `<form data-name="${addRows.text}" class="grid grid-cols-12  border-gray-500 "></form>.`
+  );
+
+  let hours = $(
+    `<div class="hour">${addRows.text}</div>`
+  );
+
+  let textArea = $(
+    `<textarea name="${addRows.text
+    }" class="textarea ${color(
+      addRows
+    )}">${store.getItem(addRows.text) || ""}</textarea>`
+  );
+
+  textArea.keydown((e) => {
+    if (e.keyCode == 13 && !e.shiftKey) {
+      e.preventDefault();
+      return false;
+    }
+  })
+}
+addRows()
+
+//Colorcode function
+
+function color(hours) {
+  let currentHour = dayjs().format('h A');
+  if (hours == currentHour) { "present" }
+  else if (hours < currentHour) { "past" }
+  else { "future" }
+}
